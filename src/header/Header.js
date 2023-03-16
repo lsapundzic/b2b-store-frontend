95392677874f2559f59419c17d76c9772bcc02a4
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-03-16 23:52:36 +0100

    The very basics of header

--- src/header/Header.js
+++ src/header/Header.js
@@ -4,15 +4,11 @@ const Header = () => {
   return (
     <div>
       <div className="header-top">
-        <div className="header-top-left"></div>
-        <div className="header-top-center"></div>
-        <div className="header-top-right"></div>
+        <div className="header-top-left">LOGO</div>
+        <div className="header-top-center">Search Bar</div>
+        <div className="header-top-right">Actions</div>
       </div>
-      <nav>
-        <ul>
-          <li></li>
-        </ul>
-      </nav>
+      <nav></nav>
     </div>
   );
 };
