be18420c2e2c31a37f50e3405e28dd8b47b58849
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-03-18 23:31:31 +0100

    tried installing latest react scripts

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
+// Router import
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 // Page imports
-import Header from "./header/Header";
-import Footer from "./components/footer/Footer";
 
 // Style imports
 import "./styles/App.css";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/products">Products</Link>
+      </div>
+
+      <Routes>
+        <Route path="./pages/Products.jsx" element={<Products />} />
+        <Route path="./pages/Home.jsx" element={<Home />} />
+      </Routes>
+
+      <div>
+        <i>This is my app</i>
+      </div>
+    </Router>
   );
 }
 
