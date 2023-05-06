12f3ec123ad550ad8bddad6c48e6f6e0fa378fb7
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-05-06 23:27:44 +0200

    Clearfix added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,18 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header>
-      <div className="banner">Header</div>
+      <div className="banner">
+        <span>
+          <img
+            src="/public/logo/png/logo-color.png"
+            alt="company logo"
+            width="280px"
+          />
+        </span>
+        <span>
+          <h1>Sotla Laboratory Experts</h1>
+        </span>
+      </div>
       <nav>
         <ul>
           <li></li>
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+// CSS global import
+import "/src/styles/global.css";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
