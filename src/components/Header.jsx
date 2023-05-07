8ddf61e704e2575aa86f2679ef71cf49e5b5fcab
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-05-07 14:22:26 +0200

    Switched to NextJS, skeleton

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,14 @@
-import { Link } from "react-router-dom";
+import Head from "next/head";
 
 export default function Header() {
   return (
     <header>
-      <div className="banner">
-        <span>
-          <img
-            src="/public/logo/png/logo-color.png"
-            alt="company logo"
-            width="280px"
-          />
-        </span>
-        <span>
-          <h1>Sotla Laboratory Experts</h1>
-        </span>
-      </div>
-      <nav>
-        <ul>
-          <li></li>
-        </ul>
-      </nav>
+      <Head>
+        <title>Sotla Lab</title>
+        <link rel="icon" href="/public/logo"></link>
+      </Head>
+      <h1>Welcome to Sotla Laboratory Experts</h1>
+      <nav>Navigation</nav>
     </header>
   );
 }
