25f28305237ccd42a7166eaa7f28992c3af87086
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-05-07 21:34:03 +0200

    Explored getting API, not working

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,20 +1,23 @@
-import Header from "@/components/header/Header.jsx";
+import Header from "@/components/header/Header";
 import axios from "axios";
 import { useState } from "react";
 
 export default function About() {
-  //   [data, getData] = useState();
+  const [data, setData] = useState([]);
+
+  useState(() => {
+    axios.get("http://localhost:1337/api/about").then((response) => {
+      console.log("Response data: ", response.data);
+      setData(response);
+    });
+  });
+
+  console.log("Data below: ", data.data.data.attributes.title);
 
   return (
     <>
       <Header />
-      <main>
-        <h2>Title</h2>
-        <p>Paragraph1</p>
-        <p>Image1</p>
-        <p>Paragraph2</p>
-        <p>Image2</p>
-      </main>
+      <main></main>
     </>
   );
 }
