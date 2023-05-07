91e2719e4eb6f3cbcffd09d7e9d63590524d8e7b
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-05-07 17:40:12 +0200

    Removed the pages that I am not working on

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+// Global style import
+import "/src/styles/globals.css";
+
+// Next imports
+import Link from "next/link";
+
+// Fragments import
+import Banner from "/src/components/Header";
+
+export default function Header() {
+  return <header></header>;
+}
