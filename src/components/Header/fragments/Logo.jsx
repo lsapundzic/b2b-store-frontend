740ced7480db0d4959f29957fc76c8a3d97ec120
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-05-07 19:35:34 +0200

    Made the logo clickable

--- src/components/Header/fragments/Logo.jsx
+++ src/components/Header/fragments/Logo.jsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Logo() {
   const imageSize = 170;
   return (
     <div>
-      <Image
-        src="/img/logo-no-background.png"
-        alt="Sotla company logo"
-        width={imageSize}
-        height={imageSize}
-      />
+      <Link href="/">
+        <Image
+          src="/img/logo-no-background.png"
+          alt="Sotla company logo"
+          width={imageSize}
+          height={imageSize}
+        />
+      </Link>
     </div>
   );
 }
