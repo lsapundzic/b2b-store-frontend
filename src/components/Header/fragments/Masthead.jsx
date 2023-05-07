fe1af10bdd4aa58076dd9a103faae5bf7db2f1f2
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-05-07 18:58:44 +0200

    Dedicated logo branch

--- src/components/Header/fragments/Masthead.jsx
+++ src/components/Header/fragments/Masthead.jsx
@@ -2,7 +2,7 @@ import Logo from "./Logo";
 
 export default function Masthead() {
   return (
-    <div className="h-40 bg-yellow-400">
+    <div className="h-38 bg-yellow-400">
       <Logo />
     </div>
   );
