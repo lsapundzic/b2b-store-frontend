ff855c44ac3f1b7d2372b281639000eb354826ef
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-05-07 21:44:25 +0200

    disabled api for now

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,16 +3,16 @@ import axios from "axios";
 import { useState } from "react";
 
 export default function About() {
-  const [data, setData] = useState([]);
+  //   const [data, setData] = useState([]);
 
-  useState(() => {
-    axios.get("http://localhost:1337/api/about").then((response) => {
-      console.log("Response data: ", response.data);
-      setData(response);
-    });
-  });
+  //   useState(() => {
+  //     axios.get("http://localhost:1337/api/about").then((response) => {
+  //       console.log("Response data: ", response.data);
+  //       setData(response);
+  //     });
+  //   });
 
-  console.log("Data below: ", data.data.data.attributes.title);
+  //   console.log("Data below: ", data.data.data.attributes.title);
 
   return (
     <>
