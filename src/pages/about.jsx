8e9710765c4482b41961b11da7664e67bbc21314
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-05-11 16:14:06 +0200

    Testing layout

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,11 +1,6 @@
-import Header from "@/components/header/Header";
-import axios from "axios";
-import { useState } from "react";
-
 export default function About() {
   return (
     <>
-      <Header />
       <main></main>
     </>
   );
