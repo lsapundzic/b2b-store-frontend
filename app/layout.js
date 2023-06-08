ff3c3f83f7eaf4c2d08edb799e97f5efbe01ae7e
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-06-08 22:53:40 +0200

    Lazy layout attempt

--- app/layout.js
+++ app/layout.js
@@ -13,11 +13,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <div className="flex min-h-screen flex-col">
+        <header className=" p-4">
+          <Navbar />
+        </header>
+        <div className="flex flex-1 flex-row">
+          <main className="flex-1 bg-blue-50 p-4">{children}</main>
+          <nav className="order-first w-32 bg-blue-100 p-4">Navigation</nav>
+          <aside className="w-32 bg-blue-100 p-4">Side</aside>
+        </div>
+        <footer className="bg-blue-200 p-4">Footer</footer>
+      </div>
+
       {/* Suppress Hydration Warning added to stop the browser console message caused by Grammarly*/}
-      <body suppressHydrationWarning={true}>
-        <Navbar />
-        <main>{children}</main>
-      </body>
+      <body suppressHydrationWarning={true}></body>
     </html>
   );
 }
