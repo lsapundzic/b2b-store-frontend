0adc7fd6d681062f25dab96ea9b9cb550bc28734
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-06-10 14:51:02 +0200

    Not happy with mui

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,8 @@
 const Navbar = () => {
+
   return (
-    <div className="navbar bg-base-100">
-      <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-    </div>
-  );
+    
+  )
 };
 
 export default Navbar;
