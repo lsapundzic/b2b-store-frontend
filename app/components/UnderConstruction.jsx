fa01128ed780fe5ea9b5bdcd972d196a1b4a91bc
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-06-10 14:51:12 +0200

    Not happy with mui!

--- app/components/UnderConstruction.jsx
+++ app/components/UnderConstruction.jsx
@@ -1,17 +1,8 @@
+"use client";
+
+import * as React from "react";
+import Button from "@mui/material/Button";
+
 export default function UnderConstruction() {
-  return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there!</h1>
-          <p className="py-6">
-            This page is under construction. Thank you for visiting but please
-            come back later.
-          </p>
-          {/* Under construction, button not necessary */}
-          {/* <button className="btn btn-primary">Get Started</button> */}
-        </div>
-      </div>
-    </div>
-  );
+  <Button variant="outlined">Button</Button>;
 }
--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,4 @@ import UnderConstruction from "./components/UnderConstruction";
 
 export default function Homepage() {
   console.log("Main page running...");
-  return (
-    <div>
-      <div className="ml-52 mr-52">
-        <UnderConstruction />
-      </div>
-    </div>
-  );
 }
