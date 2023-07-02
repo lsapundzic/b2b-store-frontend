f309aad62f840e16974180a799675ea51ce829ca
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-07-02 19:33:14 +0200

    routing navbar failed

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,76 +1,15 @@
 "use client";
 
-import {
-  AppstoreOutlined,
-  MailOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
-
-import { Button, Menu } from "antd";
+import { Menu } from "antd";
 import { useState } from "react";
 
-const items = [
-  {
-    label: "Navigation One",
-    key: "mail",
-    icon: <MailOutlined />,
-  },
-  {
-    label: "Navigation Two",
-    key: "app",
-    icon: <AppstoreOutlined />,
-    disabled: true,
-  },
-  {
-    label: "Navigation Three - Submenu",
-    key: "SubMenu",
-    icon: <SettingOutlined />,
-    children: [
-      {
-        type: "group",
-        label: "Item 1",
-        children: [
-          {
-            label: "Option 1",
-            key: "setting:1",
-          },
-          {
-            label: "Option 2",
-            key: "setting:2",
-          },
-        ],
-      },
-      {
-        type: "group",
-        label: "Item 2",
-        children: [
-          {
-            label: "Option 3",
-            key: "setting:3",
-          },
-          {
-            label: "Option 4",
-            key: "setting:4",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    label: (
-      <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-        Navigation Four - Link
-      </a>
-    ),
-    key: "alipay",
-  },
-];
+export default function Navbar({ routes }) {
+  console.log("Navbar running");
 
-export default function Navbar() {
   // Use State
   const [current, setCurrent] = useState("mail");
 
-  //   onClick event
+  //   It highlights the tab that is currently used
   const onClick = (e) => {
     console.log("click ", e);
     setCurrent(e.key);
@@ -82,7 +21,7 @@ export default function Navbar() {
         onClick={onClick}
         selectedKeys={[current]}
         mode="horizontal"
-        items={items}
+        items={routes}
       ></Menu>
     </div>
   );
--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import { Inter } from "next/font/google";
+import Navbar from "./components/navbar";
+import routes from "./routes/routes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <header></header>
+        <header>
+          <Navbar routes={routes}></Navbar>
+        </header>
         {children}
       </body>
     </html>
