73b9d28ee210cd5891c21a71386d86835dd99a3b
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-07-02 22:32:15 +0200

    Attempting to fix the header issue, no success

--- app/layout.js
+++ app/layout.js
@@ -4,11 +4,10 @@
 import {Inter} from "next/font/google";
 
 // Project components
-import Navbar from "./components/Navbar";
-import CustomFooter from "./components/Footer";
-
-
-import {Layout} from "antd";
+import {Layout, Space} from "antd";
+import Banner from "@/app/components/Banner";
+import Navbar from "@/app/components/Navbar";
+import CustomFooter from "@/app/components/Footer";
 
 const {Header, Footer, Content} = Layout;
 
@@ -20,45 +19,25 @@ export const metadata = {
         "Manufacturers, distributors, and sellers of laboratory equipment & lab consumables",
 };
 
-// Layout styling
-const headerStyle = {
-    // textAlign: "center",
-    // color: "#fff",
-    // height: 64,
-    // paddingInline: 50,
-    // lineHeight: "64px",
-    // backgroundColor: "#7dbcea",
-};
-
-const contentStyle = {
-    // textAlign: "center",
-    // minHeight: 120,
-    // lineHeight: "120px",
-    // color: "#fff",
-    // backgroundColor: "#108ee9",
-};
-
-const footerStyle = {
-    // textAlign: "center",
-    // color: "#fff",
-    // backgroundColor: "#7dbcea",
-};
-
 // TODO: Something in the header layout is giving Navbar a padding that looks terrible
 
 export default function RootLayout({children}) {
     return (
         <html lang="en">
         <body className={inter.className}>
-        <Layout>
-            <Header style={headerStyle}>
-                <Navbar/>
-            </Header>
-            <Content style={contentStyle}>{children}</Content>
-            <Footer style={footerStyle}>
-                <CustomFooter/>
-            </Footer>
-        </Layout>
+        <Space>
+            <Layout>
+                <Header>
+                    {/*<Banner content="Welcome to the big summer sale!"/>*/}
+                    <Navbar/>
+                </Header>
+                <Content>{children}</Content>
+                <Footer>
+                    {/*<CustomFooter/>*/}
+                </Footer>
+            </Layout>
+        </Space>
+
         </body>
         </html>
     );
