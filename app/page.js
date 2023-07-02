dfb460d70140c28f32c81bd008ce0394a0bf0b09
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-07-02 17:35:23 +0200

    Testing antd

--- app/page.js
+++ app/page.js
@@ -1,5 +1,13 @@
+"use client";
+
+import { Button } from "antd";
+
 export default function Home() {
   console.log("Homepage running...");
 
-  return <main></main>;
+  return (
+    <main className="App">
+      <Button>Click!</Button>
+    </main>
+  );
 }
