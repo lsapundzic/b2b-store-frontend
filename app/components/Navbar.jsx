4c22fbf5e45085ae29907eac7ffda50c83d84ac6
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-07-04 08:30:52 +0200

    Navbar solution: give it a dark mode

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,54 +1,54 @@
 "use client";
 
 // AntD icons
-import {AimOutlined, GiftOutlined, UserOutlined} from "@ant-design/icons";
+import { AimOutlined, GiftOutlined, UserOutlined } from "@ant-design/icons";
 
 //AntD components
-import {Menu} from "antd";
+import { Menu } from "antd";
 
 // React components
 import Link from "next/link";
-import {useState} from "react";
+import { useState } from "react";
 
 const routes = [
-    {
-        label: <Link href="/">Home</Link>,
-        key: "home",
-        icon: <AimOutlined/>,
-    },
-    {
-        label: <Link href="/about">About</Link>,
-        key: "about",
-        icon: <UserOutlined/>,
-    },
-    {
-        label: <Link href="/products">Products</Link>,
-        key: "products",
-        icon: <GiftOutlined/>,
-    },
+  {
+    label: <Link href="/">Home</Link>,
+    key: "home",
+    icon: <AimOutlined />,
+  },
+  {
+    label: <Link href="/about">About</Link>,
+    key: "about",
+    icon: <UserOutlined />,
+  },
+  {
+    label: <Link href="/products">Products</Link>,
+    key: "products",
+    icon: <GiftOutlined />,
+  },
 ];
 
 export default function Navbar() {
-    console.log("Navbar running");
+  console.log("Navbar running");
 
-    // Use State
-    const [current, setCurrent] = useState("mail");
+  // Use State
+  const [current, setCurrent] = useState("mail");
 
-    //   It changes useState which highlights the pressed tab
-    const onClick = (e) => {
-        console.log(`${e.key} has been clicked`, e);
-        setCurrent(e.key);
-    };
+  //   It changes useState which highlights the pressed tab
+  const onClick = (e) => {
+    console.log(`${e.key} has been clicked`, e);
+    setCurrent(e.key);
+  };
 
-    return (
-        <nav>
-            <Menu
-                // theme="dark"
-                onClick={onClick}
-                selectedKeys={[current]}
-                mode="horizontal"
-                items={routes}
-            />
-        </nav>
-    );
+  return (
+    <nav>
+      <Menu
+        theme="dark"
+        onClick={onClick}
+        selectedKeys={[current]}
+        mode="horizontal"
+        items={routes}
+      />
+    </nav>
+  );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,52 +1,49 @@
 "use client";
 
 // Google fonts
-import {Inter} from "next/font/google";
+import { Inter } from "next/font/google";
 
 // Project components
-import {Layout, Space} from "antd";
+import { Layout, Space } from "antd";
 import Navbar from "@/app/components/Navbar";
 
-const {Header, Footer, Content} = Layout;
+const { Header, Footer, Content } = Layout;
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: "Sotla Laboratory Experts",
-    description:
-        "Manufacturers, distributors, and sellers of laboratory equipment & lab consumables",
+  title: "Sotla Laboratory Experts",
+  description:
+    "Manufacturers, distributors, and sellers of laboratory equipment & lab consumables",
 };
 
 // TODO: Something in the header layout is giving Navbar a padding that looks terrible
 
 const headerStyle = {
-    textAlign: 'center',
-    color: '#fff',
-    height: 64,
-    paddingInline: 50,
-    lineHeight: '64px',
-    backgroundColor: '#7dbcea',
+  textAlign: "center",
+  color: "#fff",
+  height: 64,
+  paddingLeft: -50,
+  lineHeight: "64px",
+  backgroundColor: "#7dbcea",
 };
 
-export default function RootLayout({children}) {
-    return (
-        <html lang="en">
-        <body className={inter.className}>
-
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
         <Space>
-            <Layout style={headerStyle}>
-                <Header>
-                    {/*<Banner content="Welcome to the big summer sale!"/>*/}
-                    <Navbar/>
-                </Header>
-                <Content>{children}</Content>
-                <Footer>
-                    {/*<CustomFooter/>*/}
-                </Footer>
-            </Layout>
+          <Layout>
+            <Header>
+              <Navbar></Navbar>
+            </Header>
+            <Content>{children}</Content>
+            <Footer>
+              This is where the footer will be it just exists here
+            </Footer>
+          </Layout>
         </Space>
-
-        </body>
-        </html>
-    );
+      </body>
+    </html>
+  );
 }
