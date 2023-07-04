1846b2bcb15f5f1dfdf8a2440c85b94a287649d6
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-07-04 22:14:03 +0200

    Button UI test successful

--- app/page.js
+++ app/page.js
@@ -1,12 +1,26 @@
 "use client";
+
+// React imports
+import { useState } from "react";
+
+// Chakra imports
 import { Button, ButtonGroup } from "@chakra-ui/react";
 
 export default function Home() {
   console.log("Homepage running...");
 
+  const [count, setCount] = useState(1);
+
+  const click = () => {
+    setCount(count + 1);
+    console.log(`Clicked ${count} times`);
+  };
+
   return (
     <main>
-      <Button colorScheme="green">Button</Button>
+      <Button colorScheme="green" onClick={click}>
+        Click me!
+      </Button>
     </main>
   );
 }
