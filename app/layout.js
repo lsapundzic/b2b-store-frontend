ad98a9d644653cfb986521bd29947c029ee00907
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-07-05 07:57:37 +0200

    Keeping the branch empty for future merge

--- app/layout.js
+++ app/layout.js
@@ -13,10 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <header></header>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
