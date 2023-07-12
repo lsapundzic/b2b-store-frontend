6a9d59b5ba68481b8a9fa37de19e6930397ab5ec
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-07-12 20:38:24 +0200

    Fixing api call failed

--- app/page.js
+++ app/page.js
@@ -3,8 +3,12 @@ export default async function Home() {
     `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
   );
 
+  if (!data.ok) {
+    console.log(`ERROR: ${data}`);
+  }
+
   const res = await data.json();
-  // console.log(res);
+  console.log(res);
 
   return (
     <main>
