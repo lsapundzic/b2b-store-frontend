07d0cf7af22f0782cbd9b5de52656b88ab3119ba
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-07-15 16:18:09 +0200

    Data fetch success

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -13,17 +13,17 @@
 //   return res.json();
 // }
 
-const master = `master`;
 const getSpace = `https://cdn.contentful.com/spaces/${process.env.SPACE_ID}?access_token=${process.env.DELIVERY_ACCESS_TOKEN}`;
-const getContentModel = `https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/${master}/content_types?access_token=${process.env.DELIVERY_ACCESS_TOKEN}`;
+const getContentModel = `https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/master/content_types?access_token=${process.env.DELIVERY_ACCESS_TOKEN}`;
+const getContentType = `https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/master/content_types/staticPages?access_token=${process.env.DELIVERY_ACCESS_TOKEN}`;
 
 async function getData() {
-  const res = await fetch(`${getContentModel}`);
+  const res = await fetch(`${getContentType}`);
 
   if (!res.ok) {
     console.log("Error fetching data");
   } else {
-    console.log("Fetch OK?");
+    console.log("--- Fetch executed ---");
   }
 
   return res.json();
