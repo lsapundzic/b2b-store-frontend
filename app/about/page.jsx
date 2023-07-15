2a8f29e6922ff655625afed7148c381a3dd2d157
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-07-15 15:24:49 +0200

    directly testing data fetch

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,15 +1,27 @@
 // Fetching data
+// async function getData() {
+//   const res = await fetch(
+//     `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+//   );
+
+//   // Checking if the fetch is OK
+//   if (!res.ok) {
+//     console.log("Error fetching data");
+//   }
+
+//   // Function returns a JSON file of the fetch
+//   return res.json();
+// }
+
 async function getData() {
   const res = await fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+    `https://cdn.contentful.com/spaces/${process.env.CONTENTFUL_SPACE_ID}?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}`
   );
 
-  // Checking if the fetch is OK
   if (!res.ok) {
     console.log("Error fetching data");
   }
 
-  // Function returns a JSON file of the fetch
   return res.json();
 }
 
@@ -17,6 +29,8 @@ export default async function About() {
   console.log("About page running...");
 
   const data = await getData();
+
+  console.log(data);
 
   return (
     <main>
