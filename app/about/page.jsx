424af9ae522a7f3f8523bcfa80d49d22255c15ef
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-07-15 18:47:25 +0200

    First successful data fetch

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -3,14 +3,17 @@ import getData from "../api/fetchData";
 export default async function About() {
   console.log("About page running...");
 
-  const data = await getData(2);
+  const data = await getData();
 
-  console.log(data.sys);
+  console.log(data.items[0]);
+
+  const pageTitle = data.items[0].fields.title;
+  const pageBody = data.items[0].fields.body;
 
   return (
     <main>
-      <h1>Testing API calls</h1>
-      <p>Some contenet goes heere</p>
+      <h1>{pageTitle}</h1>
+      <p>{pageBody}</p>
     </main>
   );
 }
--- app/api/fetchData.jsx
+++ app/api/fetchData.jsx
@@ -2,14 +2,19 @@
 
 const baseURL = `https://cdn.contentful.com`;
 
+// Content types
 const getSpace = `${baseURL}/spaces/${process.env.SPACE_ID}?access_token=${process.env.DELIVERY_ACCESS_TOKEN}`;
 const getContentModel = `${baseURL}/spaces/${process.env.SPACE_ID}/environments/master/content_types?access_token=${process.env.DELIVERY_ACCESS_TOKEN}`;
 const getContentType = `https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/master/content_types/staticPages?access_token=${process.env.DELIVERY_ACCESS_TOKEN}`;
 
+// Entries
+// Get all entries of a space
+const getAllEntries = `${baseURL}/spaces/${process.env.SPACE_ID}/environments/master/entries?access_token=${process.env.DELIVERY_ACCESS_TOKEN}`;
+
 const fetchType = [getSpace, getContentModel, getContentType];
 
-async function getData(fetchRequest) {
-  const res = await fetch(fetchType[fetchRequest]);
+async function getData() {
+  const res = await fetch(`${getAllEntries}`);
 
   if (!res.ok) {
     console.log(`FETCH FAIL`);
