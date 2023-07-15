cee2a9f19188778686ec23193624edcab46e0f60
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-07-15 19:10:49 +0200

    Fix for vercel deployment

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,4 +1,21 @@
-import getData from "../api/fetchData";
+// import getData from "../api/fetchData";
+
+const baseURL = `https://cdn.contentful.com`;
+const getAllEntries = `${baseURL}/spaces/${process.env.SPACE_ID}/environments/master/entries?access_token=${process.env.DELIVERY_ACCESS_TOKEN}`;
+
+async function getData() {
+  const res = await fetch(`${getAllEntries}`);
+
+  if (!res.ok) {
+    console.log(`FETCH FAIL`);
+  } else {
+    console.log(`FETCH SUCCESS`);
+  }
+
+  console.log(`FETCH FINISHED`);
+
+  return res.json();
+}
 
 export default async function About() {
   console.log("About page running...");
