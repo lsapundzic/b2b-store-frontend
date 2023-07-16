55faca6ba303f0867a10d28ab6ff14dba12671b6
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-07-16 16:06:04 +0200

    Issues with contentful error 404

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,37 +1,36 @@
-import { client } from "../lib/contentful/client.js";
+"use client";
+
+// import { client } from "../lib/contentful/client.js";
 
 export default async function About() {
-  const entryID = "5wXQGH7Of5nYxWqPzLh8SM";
-
   console.log("About page running....");
-  client
-    .getEntry(`${entryID}`)
-    .then((entry) => console.log(entry))
-    .catch(console.error);
+
+  const data = await getData();
+
+  console.log(data);
 
   return (
     <main>
-      <h1>Some Title</h1>
-      <p>Some content right here</p>
+      <h1>About Page</h1>
     </main>
   );
 }
 
-// export const getStaticProps = async () => {
-//   console.log("Get props");
-//   const response = await client.getEntries({ content_type: "staticPages" });
+async function getData() {
+  // About page ID
+  // const pageID = "5wXQGH7Of5nYxWqPzLh8SM";
+  // Get About page URL
+  // const getSingleEntry = `https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/master/entries/${pageID}?access_token=${process.env.DELIVERY_ACCESS_TOKEN}`;
 
-//   if (!response.ok) {
-//     console.log("Error fetching data");
-//   } else {
-//     console.log("Fetch success");
-//     console.log(response);
-//   }
+  const res = await fetch(`${getSingleEntry}`);
 
-//   return {
-//     props: {
-//       pages: response.items,
-//       revalidated: 60,
-//     },
-//   };
-// };
+  if (!res.ok) {
+    console.log("FETCH FAIL");
+  } else {
+    console.log(`FETCH SUCCESS`);
+  }
+
+  console.log(`FETCH FINISHED`);
+
+  return res.json();
+}
