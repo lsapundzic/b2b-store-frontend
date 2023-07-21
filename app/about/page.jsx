88dcf9f82f226a1d05ce087ca16dd0902124506a
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-07-21 15:00:48 +0200

    Added comments and tried with simp xmpl

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -7,7 +7,7 @@ export default async function About() {
 
   const data = await getData();
 
-  console.log(data);
+  console.log("The fetched data is: ", data);
 
   return (
     <main>
@@ -16,11 +16,17 @@ export default async function About() {
   );
 }
 
+// * Trying to fetch data with the latest NextJS functions
+// ! This method used to work, and at some point it just decided not to.
+
 async function getData() {
   // About page ID
   // const pageID = "5wXQGH7Of5nYxWqPzLh8SM";
+
   // Get About page URL
   // const getSingleEntry = `https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/master/entries/${pageID}?access_token=${process.env.DELIVERY_ACCESS_TOKEN}`;
+
+  const getSingleEntry = `https://api.whatdoestrumpthink.com/api/v1/quotes/random`;
 
   const res = await fetch(`${getSingleEntry}`);
 
--- app/lib/fetch.js
+++ app/lib/fetch.js
@@ -1,3 +1,7 @@
+// This fetch is according ot the recommendation by the latest NextJS documentation: https://nextjs.org/docs/app/building-your-application/data-fetching/fetching
+
+// ! This fetch is currently disabled as I am trying other options
+
 async function getData() {
   // Get About page URL
   const getSingleEntry = `https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/master/entries/5wXQGH7Of5nYxWqPzLh8SM?access_token=${process.env.DELIVERY_ACCESS_TOKEN}`;
