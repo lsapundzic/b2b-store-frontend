b084db6d2feb6718754b670bf2ef322680e49d0d
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-07-21 16:56:44 +0200

    Contentful fetch somewhat works

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,39 +1,20 @@
 "use client";
 
-// import { client } from "../lib/contentful/client.js";
+const contentful = require("contentful");
+
+const client = contentful.createClient({
+  space: process.env.SPACE_ID,
+  environment: "master",
+  accessToken: process.env.DELIVERY_ACCESS_TOKEN,
+});
+
+client
+  .getEntry("5wXQGH7Of5nYxWqPzLh8SM")
+  .then((entry) => console.log(entry))
+  .catch(console.error);
 
 export default async function About() {
-  console.log("About page running....");
-
-  // * Trying to fetch data with the latest NextJS functions
-  // ! This method used to work, and at some point it just decided not to.
-
-  async function getData() {
-    // About page ID
-    const pageID = "5wXQGH7Of5nYxWqPzLh8SM";
-
-    // Get About page URL
-    const getSingleEntry = `https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/master/entries/${pageID}?access_token=${process.env.DELIVERY_ACCESS_TOKEN}`;
-
-    const res = await fetch(`${getSingleEntry}`);
-
-    if (!res.ok) {
-      // * Throwing errors is better than console.log it prevents the re-fetch (not completely)
-      throw new Error("Failed to fetch data");
-    } else {
-      console.log(`FETCH SUCCESS`);
-    }
-
-    console.log(`Fetch process finished`);
-
-    return res.json();
-  }
-
-  const data = await getData();
-
-  console.log("The fetched data is: ", data);
-
-  console.log("Process Complete");
+  console.log("About page running...");
 
   return (
     <main>
