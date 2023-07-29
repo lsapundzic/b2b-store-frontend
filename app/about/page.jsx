c0e613961b88939413d11600cf09f1def8efbda0
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-07-29 20:23:44 +0200

    next public workaround established

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -9,20 +9,23 @@ function About() {
   // const testAPI = "https://api.whatdoestrumpthink.com/api/v1/quotes/random";
 
   useEffect(() => {
-    const url = `https://cdn.contentful.com/spaces/wzh8zqkwvm1v/environments/master/entries/5wXQGH7Of5nYxWqPzLh8SM?access_token=ggKW5t86EQMqBvvLqmrC-vuCZg77_bqdNy-IxsF_lHk`;
+    const spaceid = process.env.NEXT_PUBLIC_SPACE_ID;
+    console.log("space id is: ", spaceid);
+
+    const url = `https://cdn.contentful.com/spaces/${spaceid}/environments/master/entries/5wXQGH7Of5nYxWqPzLh8SM?access_token=ggKW5t86EQMqBvvLqmrC-vuCZg77_bqdNy-IxsF_lHk`;
 
     fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
+      .then((resolve) => resolve.json())
+      .then((fetched) => {
+        setData(fetched);
         setLoading(false);
       });
   }, []);
 
-  console.log(data);
-
   if (isLoading) return <p>Loading...</p>;
-  if (!data) return <p>No profile data</p>;
+  if (!data) return <p>No data fetched</p>;
+
+  console.log(data);
 
   return (
     <div>
