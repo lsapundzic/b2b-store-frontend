6813a55f8a9fa6276b6ad4588bf24eb67a20805f
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-07-30 16:20:09 +0200

    Separate fetch seems to run

--- app/about/getData.jsx
+++ app/about/getData.jsx
@@ -1,6 +1,6 @@
 export async function getData() {
   const res = await fetch(
-    `https://api.whatdoestrumpthink.com/api/v1/quotes/random`
+    `https://cdn.contentful.com/spaces/wzh8zqkwvm1v/environments/master/entries/5wXQGH7Of5nYxWqPzLh8SM?access_token=ggKW5t86EQMqBvvLqmrC-vuCZg77_bqdNy-IxsF_lHk`
   );
 
   if (!res.ok) {
--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,26 +1,18 @@
-async function getData() {
-  const res = await fetch(
-    `https://cdn.contentful.com/spaces/wzh8zqkwvm1v/environments/master/entries/5wXQGH7Of5nYxWqPzLh8SM?access_token=ggKW5t86EQMqBvvLqmrC-vuCZg77_bqdNy-IxsF_lHk`
-  );
-
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Custom message: failed to fetch data");
-  }
-
-  return res.json();
-}
+import { getData } from "./getData";
 
 export default async function About() {
+  console.log("About running...");
   const data = await getData();
 
   console.log("Fetched data: ", data);
+
+  console.log("Separate fetch?");
 
   return (
     <main>
       <h1>About</h1>
       <p>
-        The following is the fetched on the server side - sign - :{" "}
+        The following is the fetched on the server side - separate! - :{" "}
         {data.fields.body}
       </p>
     </main>
