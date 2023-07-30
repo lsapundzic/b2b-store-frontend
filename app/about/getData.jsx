e12d73d6950c1349b97d58675621d94b1077e71d
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-07-30 17:36:28 +0200

    Trying with client side env var

--- app/about/getData.jsx
+++ app/about/getData.jsx
@@ -1,6 +1,6 @@
 export async function getData() {
   const res = await fetch(
-    `https://cdn.contentful.com/spaces/${process.env.SPACE_ID}/environments/master/entries/5wXQGH7Of5nYxWqPzLh8SM?access_token=${process.env.DELIVERY_ACCESS_TOKEN}`
+    `https://cdn.contentful.com/spaces/${process.env.NEXT_PUBLIC_SPACE_ID}/environments/master/entries/5wXQGH7Of5nYxWqPzLh8SM?access_token=${process.env.NEXT_PUBLIC_DELIVERY_ACCESS_TOKEN}`
   );
 
   if (!res.ok) {
