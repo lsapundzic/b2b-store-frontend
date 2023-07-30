eb8e2dd42728997af8c35085bc545b87655d1fc8
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-07-30 16:56:51 +0200

    Variable error again

--- app/about/getData.jsx
+++ app/about/getData.jsx
@@ -1,6 +1,6 @@
 export async function getData() {
   const res = await fetch(
-    `https://cdn.contentful.com/spaces/${process.env.NEXT_PUBLIC_SPACE_ID}/environments/master/entries/5wXQGH7Of5nYxWqPzLh8SM?access_token=${process.env.NEXT_PUBLIC_DELIVERY_ACCESS_TOKEN}`
+    `https://cdn.contentful.com/spaces/wzh8zqkwvm1v/environments/master/entries/5wXQGH7Of5nYxWqPzLh8SM?access_token=ggKW5t86EQMqBvvLqmrC-vuCZg77_bqdNy-IxsF_lHk`
   );
 
   if (!res.ok) {
