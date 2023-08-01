83a21c6d51bc5c7e2e8a8299b4a62e88ab40ffc9
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-02 00:28:28 +0200

    the global env file was not necessary

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,10 +2,10 @@ import { getSingleEntry } from "../data/getSingleEntry";
 import Body from "../components/Body";
 
 export default async function About() {
-  const testenv = process.env.ABOUT_PAGE_ID;
+  const aboutPageID = "5wXQGH7Of5nYxWqPzLh8SM";
 
   console.log("About running...");
-  const data = await getSingleEntry("5wXQGH7Of5nYxWqPzLh8SM");
+  const data = await getSingleEntry(aboutPageID);
 
   return (
     <main>
