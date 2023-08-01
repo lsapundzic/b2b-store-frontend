59c4aef615830d3d14ab6116b06404a77f7c59ef
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-01 23:34:35 +0200

    Testing use of local variables

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,11 +2,15 @@ import { getSingleEntry } from "../data/getSingleEntry";
 import Body from "../components/Body";
 
 export default async function About() {
+  const testenv = process.env.ABOUT_PAGE_ID;
+
   console.log("About running...");
   const data = await getSingleEntry("5wXQGH7Of5nYxWqPzLh8SM");
 
   return (
     <main>
+      <h1>The env is:{testenv} </h1>
+
       <Body content={data} />
     </main>
   );
