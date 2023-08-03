9cc7b008e433e30b9ba4c4cb820a69196fd1c530
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-03 20:22:35 +0200

    This version works as well

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,42 +1,32 @@
 "use client";
 
-// React imports
-import { useState, useEffect } from "react";
-
-// Project imports
+import React, { useState, useEffect } from "react";
 import { fetchData } from "../services/fetchData";
-import { ABOUT_ID } from "../services/requests";
-
-// AntD Imports
-import { Empty } from "antd";
 
 function About() {
-  console.log("About page rendered...");
   const [data, setData] = useState(null);
-  // const [isLoading, setLoading] = useState(true);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    // A
-
-    fetchData(ABOUT_ID)
+    fetchData()
       .then((data) => {
         setData(data);
-        console.log("Data fetched ", data);
+        setLoading(false);
       })
       .catch((error) => {
-        console.log("An error has interfered somewhere");
+        console.error("Error fetching data:", error);
+        setLoading(false);
       });
   }, []);
 
-  // console.log(a);
+  console.log(data);
 
-  // if (isLoading) return <p>Loading...</p>;
-  if (!data) return <Empty />;
+  if (isLoading) return <p>Loading...</p>;
+  if (!data) return <p>No profile data</p>;
 
   return (
     <div>
-      <h1>Client Side API Fetch</h1>
-      <h2>{data.fields.title}</h2>
+      <h1>{data.fields.title}</h1>
       <p>{data.fields.body}</p>
     </div>
   );
--- app/services/fetchData.jsx
+++ app/services/fetchData.jsx
@@ -1,7 +1,9 @@
-"use client";
+const apiUrl = `https://cdn.contentful.com/spaces/wzh8zqkwvm1v/environments/master/entries/5wXQGH7Of5nYxWqPzLh8SM?access_token=ggKW5t86EQMqBvvLqmrC-vuCZg77_bqdNy-IxsF_lHk`;
 
-export function fetchData(entryID) {
-  const url = `https://cdn.contentful.com/spaces/wzh8zqkwvm1v/environments/master/entries/${entryID}?access_token=ggKW5t86EQMqBvvLqmrC-vuCZg77_bqdNy-IxsF_lHk`;
-
-  return fetch(url).then((res) => res.json());
+export async function fetchData() {
+  const response = await fetch(apiUrl);
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return response.json();
 }
