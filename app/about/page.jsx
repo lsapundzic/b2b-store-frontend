be6c2f5df23a3777e4b6ad5377e2d5439c4b317c
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-03 19:57:04 +0200

    client fetch working well

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -5,40 +5,37 @@ import { useState, useEffect } from "react";
 
 // Project imports
 import { fetchData } from "../services/fetchData";
+import { ABOUT_ID } from "../services/requests";
+
+// AntD Imports
+import { Empty } from "antd";
 
 function About() {
+  console.log("About page rendered...");
   const [data, setData] = useState(null);
   // const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
-    const url = `https://cdn.contentful.com/spaces/wzh8zqkwvm1v/environments/master/entries/5wXQGH7Of5nYxWqPzLh8SM?access_token=ggKW5t86EQMqBvvLqmrC-vuCZg77_bqdNy-IxsF_lHk`;
-
-    fetchData()
+    fetchData(ABOUT_ID)
       .then((data) => {
         setData(data);
-        console.log(data);
+        console.log("Data fetched ", data);
       })
       .catch((error) => {
         console.log("An error has interfered somewhere");
       });
-
-    // fetch(url)
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     setData(data);
-    //     setLoading(false);
-    //   });
   }, []);
 
-  console.log(data);
+  // console.log(a);
 
   // if (isLoading) return <p>Loading...</p>;
-  if (!data) return <p>No profile data</p>;
+  if (!data) return <Empty />;
 
   return (
     <div>
       <h1>Client Side API Fetch</h1>
       <h2>{data.fields.title}</h2>
+      <p>{data.fields.body}</p>
     </div>
   );
 }
--- app/services/fetchData.jsx
+++ app/services/fetchData.jsx
@@ -1,7 +1,7 @@
 "use client";
 
-export function fetchData() {
-  const url = `https://cdn.contentful.com/spaces/wzh8zqkwvm1v/environments/master/entries/5wXQGH7Of5nYxWqPzLh8SM?access_token=ggKW5t86EQMqBvvLqmrC-vuCZg77_bqdNy-IxsF_lHk`;
+export function fetchData(entryID) {
+  const url = `https://cdn.contentful.com/spaces/wzh8zqkwvm1v/environments/master/entries/${entryID}?access_token=ggKW5t86EQMqBvvLqmrC-vuCZg77_bqdNy-IxsF_lHk`;
 
   return fetch(url).then((res) => res.json());
 }
