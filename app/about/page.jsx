090e01fd304d5317c3b5e69028b77c60ac00d068
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-03 18:44:54 +0200

    Decided to commit fully to CSR

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,4 +1,4 @@
-// "use client";
+"use client";
 
 import { client } from "../contentful/client.js";
 import StaticContent from "../components/StaticContent";
@@ -7,13 +7,14 @@ import { ABOUT_ID } from "../utils/settings";
 export default async function About() {
   console.log("About page rendered...");
 
-  const result = await client.getEntry(ABOUT_ID);
-
-  console.log(result);
+  client
+    .getEntry(ABOUT_ID)
+    .then((entry) => setData(entry))
+    .catch(console.error("--- --- --- ERROR FETCHING DATA --- --- ---"));
 
   return (
     <main>
-      <h1>{result.fields.title}</h1>
+      <h1>{data.title}</h1>
     </main>
   );
 }
