8da94c3c40199bd30521881f589d51221a1c82e2
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-04 23:09:22 +0200

    Updating branch to prevent error conflict

---
+++ app/components/ProductPreview.jsx
@@ -0,0 +1,22 @@
+import { Card, Image } from "antd";
+const { Meta } = Card;
+const ProductPreview = () => {
+  return (
+    <Card
+      hoverable
+      style={{
+        width: 240,
+      }}
+      cover={
+        <Image
+          alt="A sample product"
+          src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+        />
+      }
+    >
+      <Meta title="OPTIC900B" description="Binocular biological microscope" />
+    </Card>
+  );
+};
+
+export default ProductPreview;
