e7abcb4fd7e08d38939312f407419a9ef2ce51e4
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-06 22:06:15 +0200

    Created a test file for getting data

--- app/tests/page.jsx
+++ app/tests/page.jsx
@@ -1,10 +1,29 @@
-const DataTests = () => {
-  return (
-    <div>
-      <h1>Data Tests</h1>
-      <p>Data output:</p>
-    </div>
-  );
-};
+// This is only for testing API and GET requests with SSR- no interface
 
-export default DataTests;
+// Fetching data on the server side
+export async function dataFetch() {
+  const testURL = `https://cdn.contentful.com/spaces/${process.env.NEXT_PUBLIC_SPACE_ID}/environments/master/entries/5wXQGH7Of5nYxWqPzLh8SM?access_token=${process.env.NEXT_PUBLIC_DELIVERY_ACCESS_TOKEN}`;
+
+  const res = await fetch(testURL);
+
+  if (!res.ok) {
+    throw new Error("Custom message: failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Test() {
+  console.log("--- --- TESTING PLATFORM RUNNING --- ---");
+  const data = await dataFetch();
+
+  console.log("Fetched data: ", data);
+
+  console.log("------------------------------------");
+
+  return (
+    <main>
+      <h1>Testing Data Fetch</h1>
+    </main>
+  );
+}
