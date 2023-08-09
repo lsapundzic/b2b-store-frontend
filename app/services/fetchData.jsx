90c5fb1af209399bb41e3851f3199987d036e9bd
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-09 16:28:31 +0200

    Safer fetch

--- app/services/fetchData.jsx
+++ app/services/fetchData.jsx
@@ -2,13 +2,30 @@
 
 import { BASE_URL, GET_ALL_ENTRIES } from "./requests";
 
-export function fetchData(entryID) {
+export async function fetchData(entryID) {
   let url;
-  if (entryID === "all") {
+
+  if (entryID === 0) {
     url = GET_ALL_ENTRIES;
   } else {
-    url = `${BASE_URL}/spaces/${process.env.NEXT_PUBLIC_SPACE_ID}/environments/master/entries/${entryID}?access_token=${process.env.NEXT_PUBLIC_DELIVERY_ACCESS_TOKEN}`;
+    url = constructEntryURL(entryID);
   }
 
-  return fetch(url).then((res) => res.json());
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch data. Status: ${response.status}`);
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
+}
+
+// URL constructor separated from fetching for clarity
+function constructEntryURL(entryID) {
+  return `${BASE_URL}/spaces/${process.env.NEXT_PUBLIC_SPACE_ID}/environments/master/entries/${entryID}?access_token=${process.env.NEXT_PUBLIC_DELIVERY_ACCESS_TOKEN}`;
 }
