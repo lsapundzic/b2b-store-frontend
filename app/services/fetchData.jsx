54f71a03be7c17c3d29c81364c631a79c7d8d698
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-09 15:51:08 +0200

    First instance of fetching generalization

--- app/services/fetchData.jsx
+++ app/services/fetchData.jsx
@@ -1,9 +1,14 @@
 "use client";
 
-import { BASE_URL } from "./requests";
+import { BASE_URL, GET_ALL_ENTRIES } from "./requests";
 
 export function fetchData(entryID) {
-  const url = `${BASE_URL}/spaces/${process.env.NEXT_PUBLIC_SPACE_ID}/environments/master/entries/${entryID}?access_token=${process.env.NEXT_PUBLIC_DELIVERY_ACCESS_TOKEN}`;
+  let url;
+  if (entryID === "all") {
+    url = GET_ALL_ENTRIES;
+  } else {
+    url = `${BASE_URL}/spaces/${process.env.NEXT_PUBLIC_SPACE_ID}/environments/master/entries/${entryID}?access_token=${process.env.NEXT_PUBLIC_DELIVERY_ACCESS_TOKEN}`;
+  }
 
   return fetch(url).then((res) => res.json());
 }
--- app/services/requests.jsx
+++ app/services/requests.jsx
@@ -1,3 +1,9 @@
+// Page IDs
+export const ABOUT_ID = `5wXQGH7Of5nYxWqPzLh8SM`;
+export const CONTACT_ID = `2UDWb179AgOuPSwqzLytSM`;
+export const TRACKING_ID = `6xWhYn726PTNQKjnWBQJCj`;
+
+// Contentful API base URL
 export const BASE_URL = `https://cdn.contentful.com`;
 
 // SPACE
@@ -12,9 +18,5 @@ export const GET_CONTENT_TYPE = `https://cdn.contentful.com/spaces/${process.env
 // Get all entries of a space
 export const GET_ALL_ENTRIES = `${BASE_URL}/spaces/${process.env.SPACE_ID}/environments/master/entries?access_token=${process.env.DELIVERY_ACCESS_TOKEN}`;
 
+// Get a single entry based on the entryID
 export const GET_SINGLE_ENTRY = `${BASE_URL}/spaces/${process.env.SPACE_ID}/environments/master/entries/5wXQGH7Of5nYxWqPzLh8SM?access_token=${process.env.DELIVERY_ACCESS_TOKEN}`;
-
-// Static Page IDs
-export const ABOUT_ID = `5wXQGH7Of5nYxWqPzLh8SM`;
-export const CONTACT_ID = `2UDWb179AgOuPSwqzLytSM`;
-export const TRACKING_ID = `6xWhYn726PTNQKjnWBQJCj`;
