00c59a2d8d8bb147f029b010694fcf8594ba907c
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-10 23:02:27 +0200

    Touch ups

--- app/[product]/page.jsx
+++ app/[product]/page.jsx
@@ -1,11 +1,46 @@
 "use client";
 
+// React imports
+import { useState, useEffect } from "react";
+
+// Project imports
+import { fetchData } from "../services/fetchData";
+import { pageStyle, textStyle } from "../styles/globalStyles";
+
+// AntD Imports
+import { Empty, Skeleton, Typography } from "antd";
+const { Title, Paragraph } = Typography;
+
 export default function Product({ params }) {
+  console.log("Product page rendered...");
+  // This is the product ID passed from the ProductCard.jsx
   const { product } = params;
+
+  // For holding fetched data and loading state
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchData(product)
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+        console.log("Data fetched ", data);
+      })
+      .catch((error) => {
+        console.error("Failure to fetch data.", error);
+      });
+  }, [product]);
+
+  if (isLoading) return <Skeleton active />;
+  if (!data) return <Empty />;
+
   return (
-    <div>
-      <h1>Welcome to the Product page!</h1>
-      <h2>Product ID: {product}</h2>
+    <div style={pageStyle}>
+      <Typography>
+        <Title>{data.fields.name}</Title>
+        <Paragraph style={textStyle}>{data.fields.description}</Paragraph>
+      </Typography>
     </div>
   );
 }
