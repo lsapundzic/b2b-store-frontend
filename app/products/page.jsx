b194ff9903f6fbca8370fbd5198c31f7964ab9ae
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-10 14:09:02 +0200

    Placeholder image added

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -9,7 +9,7 @@ import { CONTACT_ID } from "../services/requests";
 import { pageStyle, textStyle } from "../styles/globalStyles";
 
 // AntD Imports
-import { Empty, Skeleton, Typography, Card } from "antd";
+import { Empty, Skeleton, Typography, Card, Image } from "antd";
 const { Title, Paragraph } = Typography;
 
 function Products() {
@@ -34,12 +34,23 @@ function Products() {
   if (isLoading) return <Skeleton active />;
   if (!data) return <Empty />;
 
+  // ! For development purposes
+  console.log("All the products: ", data.items);
+
+  const tempImagePlaceholder = `https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png`;
   return (
     <div style={pageStyle}>
       <Typography>
         <Title>All Products</Title>
         {data.items.map((product, index) => (
-          <Card key={index} style={{ width: 300 }}>
+          <Card
+            key={index}
+            style={{ width: 300 }}
+            hoverable
+            cover={
+              <Image alt="product image" src="http://placekitten.com/200/300" />
+            }
+          >
             <p>{product.fields.name}</p>
             <p>{product.fields.description}</p>
           </Card>
