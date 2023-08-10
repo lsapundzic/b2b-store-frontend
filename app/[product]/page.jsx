1adabfcd1d7e19a18fd2702720087cd558983ce0
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-10 17:48:25 +0200

    Passed parameters to product

--- app/[product]/page.jsx
+++ app/[product]/page.jsx
@@ -1,5 +1,11 @@
 "use client";
 
-export default function Product() {
-  return <h1>Welcome to the Product page!</h1>;
+export default function Product({ params }) {
+  const { product } = params;
+  return (
+    <div>
+      <h1>Welcome to the Product page!</h1>
+      <h2>Product ID: {product}</h2>
+    </div>
+  );
 }
