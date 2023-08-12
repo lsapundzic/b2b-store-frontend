dcda9198aeae00f5c485342cbedc87cdf565314e
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-12 12:37:06 +0200

    Pushing to deployment before the break

--- app/tests/page.jsx
+++ app/tests/page.jsx
@@ -1,14 +1,5 @@
 // This is only for testing API and GET requests with SSR- no interface
 
-import { Image } from "antd";
-import {
-  GET_ALL_ENTRIES,
-  GET_CONTENT_MODEL,
-  GET_CONTENT_TYPE,
-  GET_CONTENT_TYPE_ENTRIES,
-  GET_SPACE,
-} from "../services/requests";
-
 // Fetching data on the server side
 export async function dataFetch(fetchMe) {
   const res = await fetch(fetchMe);
@@ -46,7 +37,7 @@ export default async function TestPage() {
   const data = await dataFetch(retrievalURL);
 
   console.log(now, "--- Fetched data: --- ", data.fields.file.url);
-  const imageURL = data.fields.file.url.toString();
+  const imageURL = data.fields.file.url;
   console.log("What is the URL? ", imageURL);
 
   console.log("------------------------------------");
