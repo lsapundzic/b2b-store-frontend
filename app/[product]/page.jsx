1a03493add9478ec9ede3fa7adf20947de1832de
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-14 20:40:29 +0200

    Individual product page custom hook enabled

--- app/[product]/page.jsx
+++ app/[product]/page.jsx
@@ -1,12 +1,11 @@
 "use client";
 
 // React imports
-import { useState, useEffect } from "react";
+import { usePageContent } from "../hooks/usePageContent.jsx";
 
 // Project imports
-import { fetchData } from "../services/fetchData";
 import { pageStyle, textStyle } from "../styles/globalStyles";
-import { SingleAssetURL, SingleEntryURL } from "../utils/buildURL";
+import { SingleEntryURL } from "../utils/buildURL";
 
 // AntD Imports
 import { Empty, Skeleton, Typography } from "antd";
@@ -14,26 +13,10 @@ import ImageGenerator from "../utils/ImageGenerator";
 const { Title, Paragraph } = Typography;
 
 export default function Product({ params }) {
-  console.log("Product page rendered...");
   const { product } = params;
+  console.log("Product page rendered...");
 
-  const [data, setData] = useState(null);
-
-  // For toggling general loading status
-  const [isLoading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Fetch page content
-    fetchData(SingleEntryURL(product))
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-        console.log("Data fetched ", data);
-      })
-      .catch((error) => {
-        console.error("Failure to fetch data.", error);
-      });
-  }, [product]);
+  const { data, isLoading } = usePageContent(SingleEntryURL(product));
 
   if (isLoading) return <Skeleton active />;
   if (!data) return <Empty />;
