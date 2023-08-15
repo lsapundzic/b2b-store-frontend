171a4a4d87600205f8ec3796e4962ef3aaebf687
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-15 11:33:46 +0200

    Testing ant design components

--- app/products/[product]/page.jsx
+++ app/products/[product]/page.jsx
@@ -15,7 +15,7 @@ import { SingleEntryURL } from "../../utils/buildURL.jsx";
 import ImageAsset from "../../utils/ImageAsset.jsx";
 
 // AntD imports
-import { Col, Empty, Row, Skeleton, Typography } from "antd";
+import { Button, Col, Divider, Empty, Row, Skeleton, Typography } from "antd";
 const { Title, Paragraph } = Typography;
 
 export default function Product({ params }) {
@@ -40,13 +40,55 @@ export default function Product({ params }) {
         </Col>
         <Col span={12}>
           <Typography>
-            <Title>
+            <Title level={4}>
               {data.fields.name} - {data.fields.fullName}
             </Title>
+            <Divider />
             <Paragraph style={textStyle}>{data.fields.description}</Paragraph>
+            <Divider />
+            <Button
+              type="primary"
+              style={{
+                width: `100%`,
+                height: 45,
+                backgroundColor: "green",
+              }}
+            >
+              Contact Sales Support
+            </Button>
           </Typography>
         </Col>
         <Col span={4}>Conditions card will go here</Col>
+      </Row>
+      <Row>
+        <Col span={15}>
+          <table>
+            <thead>
+              <tr>
+                <th>Header 1</th>
+                <th>Header 2</th>
+                <th>Header 3</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Row 1, Cell 1</td>
+                <td>Row 1, Cell 2</td>
+                <td>Row 1, Cell 3</td>
+              </tr>
+              <tr>
+                <td>Row 2, Cell 1</td>
+                <td>Row 2, Cell 2</td>
+                <td>Row 2, Cell 3</td>
+              </tr>
+              <tr>
+                <td>Row 3, Cell 1</td>
+                <td>Row 3, Cell 2</td>
+                <td>Row 3, Cell 3</td>
+              </tr>
+            </tbody>
+          </table>
+        </Col>
       </Row>
     </div>
   );
