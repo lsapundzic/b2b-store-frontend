cdc9864553287311530906c9b76d4c0dcb323979
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-19 21:14:25 +0200

    Enabled contact form, looks attrocious

--- app/components/DetailedContact.jsx
+++ app/components/DetailedContact.jsx
@@ -8,36 +8,52 @@ export default function DetailedContact({ sectionTitle }) {
   return (
     <div>
       <Title style={{ textAlign: "center" }}>{sectionTitle}</Title>
-      <Space direction="vertical">
-        <Title level={3}>Direct Contact</Title>
-        <div>office@sotla.si</div>
-        <div>+386 852 547 593</div>
-      </Space>
-      <Space direction="vertical">
-        <Title level={3}>Headquarters</Title>
-        <div>office@sotla.si</div>
-        <div>+386 852 547 593</div>
-      </Space>
-      <Space direction="vertical">
-        <Title level={3}>Work With Us</Title>
-        <div>office@sotla.si</div>
-        <div>+386 852 547 593</div>
-      </Space>
-      <Space direction="vertical">
-        <Title level={3}>Customer Service</Title>
-        <div>office@sotla.si</div>
-        <div>+386 852 547 593</div>
-      </Space>
-      <Space direction="vertical">
-        <Title level={3}>Work With Us</Title>
-        <div>office@sotla.si</div>
-        <div>+386 852 547 593</div>
-      </Space>
-      <Space direction="vertical">
-        <Title level={3}>Work With Us</Title>
-        <div>office@sotla.si</div>
-        <div>+386 852 547 593</div>
-      </Space>
+      <Row>
+        <Col span={8} style={{ textAlign: "center" }}>
+          <Space direction="vertical">
+            <Title level={3}>Direct Contact</Title>
+            <div>office@sotla.si</div>
+            <div>+386 852 547 593</div>
+          </Space>
+        </Col>
+        <Col span={8} style={{ textAlign: "center" }}>
+          <Space direction="vertical">
+            <Title level={3}>Headquarters</Title>
+            <div>office@sotla.si</div>
+            <div>+386 852 547 593</div>
+          </Space>
+        </Col>
+        <Col span={8} style={{ textAlign: "center" }}>
+          <Space direction="vertical">
+            <Title level={3}>Work With Us</Title>
+            <div>office@sotla.si</div>
+            <div>+386 852 547 593</div>
+          </Space>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={8} style={{ textAlign: "center" }}>
+          <Space direction="vertical">
+            <Title level={3}>Customer Service</Title>
+            <div>office@sotla.si</div>
+            <div>+386 852 547 593</div>
+          </Space>
+        </Col>
+        <Col span={8} style={{ textAlign: "center" }}>
+          <Space direction="vertical">
+            <Title level={3}>Media Relations</Title>
+            <div>office@sotla.si</div>
+            <div>+386 852 547 593</div>
+          </Space>
+        </Col>
+        <Col span={8} style={{ textAlign: "center" }}>
+          <Space direction="vertical">
+            <Title level={3}>Distributor Support</Title>
+            <div>office@sotla.si</div>
+            <div>+386 852 547 593</div>
+          </Space>
+        </Col>
+      </Row>
     </div>
   );
 }
