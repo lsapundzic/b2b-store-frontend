fd28bb44e7d13d8a79e1453938874499a94539f8
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-22 22:13:10 +0200

    layout and page complete

--- app/page.js
+++ app/page.js
@@ -1,68 +1,66 @@
 "use client";
 
-// React
-import { useContext } from "react";
+import React from "react";
+import {
+  Carousel,
+  Col,
+  ConfigProvider,
+  Divider,
+  Image,
+  Row,
+  Typography,
+} from "antd";
 
 // Project
 import { pageStyle } from "./styles/globalStyles";
-import { PageCategoryContext } from "./layout";
 
 // AntD
-import { Carousel, Col, Divider, Image, Row, Typography } from "antd";
-import ImageAsset from "./components/ImageAsset";
+import MainLayout from "./components/MainLayout.jsx";
+
 const { Title } = Typography;
 
-const contentStyle = {
-  height: "400px",
-  color: "#fff",
-  lineHeight: "160px",
-  textAlign: "center",
-  background: "green",
-};
-
 export default function Homepage() {
-  const { pageCategory } = useContext(PageCategoryContext);
-  console.log(`${pageCategory} rendered`);
-
   return (
-    <div style={pageStyle}>
-      <Row>
-        <Col xs={0} sm={0} md={24}>
-          <Carousel autoplay>
-            <div>
-              <Image
-                src="/images/home/carousel-1.jpg"
-                alt="placeholder image"
-                width={"auto"}
-                height={"auto"}
-                preview={false}
-              />
-            </div>
-            <div>
-              <Image
-                alt="A placeholder image"
-                src="https://picsum.photos/1800/600"
-                preview={false}
-              />
-            </div>
-            <div>
-              <Image
-                alt="A placeholder image"
-                src="https://placehold.co/1800x600"
-                preview={false}
-              />
-            </div>
-          </Carousel>
-        </Col>
-      </Row>
-      <Divider />
-      <Row>
-        <Col>
-          <Typography>
-            <Title level={2}>Special Offers</Title>
-          </Typography>
-        </Col>
-      </Row>
-    </div>
+    <MainLayout>
+      <div style={pageStyle}>
+        <Row>
+          <Col xs={0} sm={0} md={24}>
+            <Carousel autoplay>
+              <div>
+                <Image
+                  src="/images/home/carousel-1.jpg"
+                  alt="placeholder image"
+                  width={"auto"}
+                  height={"auto"}
+                  preview={false}
+                />
+              </div>
+              <div>
+                <Image
+                  alt="A placeholder image"
+                  src="https://picsum.photos/1800/600"
+                  preview={false}
+                />
+              </div>
+              <div>
+                <Image
+                  alt="A placeholder image"
+                  src="https://placehold.co/1800x600"
+                  preview={false}
+                />
+              </div>
+            </Carousel>
+          </Col>
+        </Row>
+        <Divider />
+        <Row>
+          <Col>
+            <Typography>
+              <Title level={2}>Special Offers</Title>
+            </Typography>
+          </Col>
+        </Row>
+      </div>
+    </MainLayout>
   );
 }
