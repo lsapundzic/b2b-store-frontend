6abc740edae15afbde5218a6ef9a3bef111a518c
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-22 21:47:33 +0200

    added theme, lib, about

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -5,18 +5,16 @@ import { useDataRetriever } from "../hooks/useDataRetriever.jsx";
 import { useContext } from "react";
 
 // Project
-import { pageStyle, textStyle } from "../styles/globalStyles.js";
+import { pageStyle } from "../styles/globalStyles.js";
 import { ABOUT_ID, SingleEntryURL } from "../utils/buildURL";
-import { PageCategoryContext } from "../layout.js";
+import MainLayout, { PageCategoryContext } from "../components/MainLayout.jsx";
 import CustomSkeleton from "../components/CustomSkeleton.jsx";
-import CustomEmpty from "../components/CustomEmpty.jsx";
 import CustomBanner from "../components/CustomBanner.jsx";
 import TeamMembers from "../components/TeamMembers.jsx";
 
 // AntD
-import { Col, Divider, Image, Row, Typography } from "antd";
+import { Col, Divider, Image, Row } from "antd";
 import Certificates from "../components/Certificates.jsx";
-const { Title, Paragraph } = Typography;
 
 export default function About() {
   let { pageCategory } = useContext(PageCategoryContext);
@@ -24,34 +22,34 @@ export default function About() {
 
   const { data, isLoading } = useDataRetriever(SingleEntryURL(ABOUT_ID));
 
-  if (isLoading)
-    return (
-      <div style={pageStyle}>
-        <CustomSkeleton />
-      </div>
-    );
-  if (!data) return <CustomEmpty />;
-
   return (
-    <div style={pageStyle}>
-      <Row>
-        <Col xs={0} sm={0} md={24}>
-          <Image
-            alt="Placeholder team picture"
-            src="http://placekitten.com/1800/400"
-            // width={1800}
-            // height={600}
-            preview={false}
-          />
-        </Col>
-      </Row>
-      <TeamMembers
-        componentTitle={data.fields.title}
-        componentBody={data.fields.body}
-      />
-      <CustomBanner bannerContent={""} />
-      <Divider orientation="left">Certificates</Divider>
-      <Certificates />
-    </div>
+    <MainLayout>
+      <div style={pageStyle}>
+        {isLoading || !data ? (
+          <CustomSkeleton />
+        ) : (
+          <>
+            <Row>
+              <Col xs={0} sm={0} md={24}>
+                <Image
+                  alt="Placeholder team picture"
+                  src="http://placekitten.com/1800/400"
+                  // width={1800}
+                  // height={600}
+                  preview={false}
+                />
+              </Col>
+            </Row>
+            <TeamMembers
+              componentTitle={data.fields.title}
+              componentBody={data.fields.body}
+            />
+            <CustomBanner bannerContent={""} />
+            <Divider orientation="left">Certificates</Divider>
+            <Certificates />
+          </>
+        )}
+      </div>
+    </MainLayout>
   );
 }
