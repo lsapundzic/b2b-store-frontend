96c3978ce68b6173dda9d19f591eb355053c589d
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-26 19:35:03 +0200

    Responsive design fully implemented

--- app/components/CustomHeader.jsx
+++ app/components/CustomHeader.jsx
@@ -1,13 +1,11 @@
 "use client";
 
-// React
-import Image from "next/image";
 
-// AntD
-import Link from "antd/es/typography/Link";
-import Title from "antd/es/typography/Title";
-import {Col, Row} from "antd";
 import useWindowDimension from "@/app/hooks/useWindowDimension";
+import {Col, Row} from "antd"
+import Link from "next/link"
+import Image from "next/image"
+import Title from "antd/es/typography/Title";
 
 const customHeaderStyle = {
     backgroundColor: "white", paddingLeft: 50, paddingRight: 50,
@@ -16,28 +14,45 @@ const customHeaderStyle = {
 export default function CustomHeader() {
     let {width, height} = useWindowDimension();
 
-    console.log("Width", width, "Height", height);
+
+    return (
+        <div>
+            {/* Conditionally render a component based on window width */}
+            {width < 660 ? (
+                <Row align="middle" style={customHeaderStyle}>
+                    <Col flex="auto">
+                        <Link href="/">
+                            <Title level={2} style={{alignContent: "center"}}>
+                                Sotla Laboratory Equipment
+                            </Title>
+                        </Link>
+                    </Col>
+                </Row>
+            ) : (
+                <Row align="bottom" style={customHeaderStyle}>
+                    <Col flex="110px">
+                        <Link href="/">
+                            <Image
+                                src="/icons/png/logo-no-background.png"
+                                alt="Company Logo"
+                                width={110}
+                                height={70}
+                            />
+                        </Link>
+                    </Col>
+                    <Col flex="auto">
+                        <Link href="/">
+                            <Title level={2} style={{alignContent: "center"}}>
+                                Sotla Laboratory Equipment
+                            </Title>
+                        </Link>
+                    </Col>
+                </Row>
+            )}
+        </div>
+    )
 
 
-    return (<Row align="middle" style={customHeaderStyle}>
-        <Col flex="110px">
-            <Link href="/">
-                <Image
-                    src="/icons/png/logo-no-background.png"
-                    alt="Company Logo"
-                    width={110}
-                    height={70}
-                />
-            </Link>
-        </Col>
-        <Col flex="auto">
-            <Link href="/">
-                <Title level={2} style={{alignContent: "center"}}>
-                    Sotla Laboratory Equipment
-                </Title>
-            </Link>
-        </Col>
-
-
-    </Row>);
 }
+
+
