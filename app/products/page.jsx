4239d97045c983018565dee44ae12353a9a8379c
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-26 16:07:19 +0200

    I am not hating this solution

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,77 +1,25 @@
-/*
-- Products.jsx fetches all Contentful entries of the "product" content type
-- Each entry has a unique ID 
-- Each entry has an asset (image) that has its own unique ID
-- .map function generates ProductCard components for each entry
-- Each ProductCard is passed entryID which will be used in [product].jsx to fetch that page
-- Each ProductCard is passed assetID which will be used by ImageAsset.jsx to generate an image
-*/
-
 "use client";
 
-// React
-import { useContext, useEffect } from "react";
+
+import {useContext} from "react";
 import MainLayout from "../MainLayout.jsx";
-import { PageCategoryContext } from "../layout.js";
-import { useDataRetriever } from "../hooks/useDataRetriever.jsx";
-
-// Project
-import { pageStyle } from "../styles/globalStyles";
-import ProductCard from "../components/ProductCard";
-import { ContentTypeEntriesURL } from "../utils/buildURL";
-import useFilterProducts from "../hooks/useFilterProducts.jsx";
-import CustomSkeleton from "../components/CustomSkeleton.jsx";
-
-// AntD
-import { Row, Col } from "antd";
+import {PageCategoryContext} from "../layout.js";
 import ProductsDisplayWindow from "@/app/components/ProductsDisplayWindow";
+import {Col, Row} from "antd";
 
 function Products() {
-  let { pageCategory } = useContext(PageCategoryContext);
-  console.log(`${pageCategory} rendered`);
+    let {pageCategory} = useContext(PageCategoryContext);
+    console.log(`${pageCategory} rendered`);
 
-  // Hook for getting data
-  const { data, isLoading } = useDataRetriever(
-    ContentTypeEntriesURL("product")
-  );
-
-  // Hook for filtering data
-  const { filteredData } = useFilterProducts(data, pageCategory);
-
-  return (
-    <MainLayout>
-      {/*<div style={pageStyle}>*/}
-      {/*  {isLoading || !data ? (*/}
-      {/*    <CustomSkeleton />*/}
-      {/*  ) : (*/}
-      {/*    <>*/}
-      {/*      <Row gutter={[32, 64]}>*/}
-      {/*        /!* For filtering products based on their category / navbar option *!/*/}
-      {/*        {filteredData.map((product) => (*/}
-      {/*          <Col*/}
-      {/*            key={product.sys.id}*/}
-      {/*            span={{*/}
-      {/*              xs: 24,*/}
-      {/*              sm: 12,*/}
-      {/*              md: 8,*/}
-      {/*              lg: 4,*/}
-      {/*            }}*/}
-      {/*          >*/}
-      {/*            <ProductCard*/}
-      {/*              product={product}*/}
-      {/*              cardWidth={300}*/}
-      {/*              cardHeight={470}*/}
-      {/*            />*/}
-      {/*          </Col>*/}
-      {/*        ))}*/}
-      {/*      </Row>*/}
-      {/*    </>*/}
-      {/*  )}*/}
-      {/*</div>*/}
-
-        <ProductsDisplayWindow category={pageCategory} />
-    </MainLayout>
-  );
+    return (
+        <MainLayout>
+            <Row>
+                <Col span={24}>
+                    <ProductsDisplayWindow category={pageCategory}/>
+                </Col>
+            </Row>
+        </MainLayout>
+    );
 }
 
 export default Products;
