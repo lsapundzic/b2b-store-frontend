b1fa28dd4544579410c687c10a508ce8f464e9c7
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-27 18:16:21 +0200

    All pages enabled as templates

--- app/cooperation/terms/page.jsx
+++ app/cooperation/terms/page.jsx
@@ -1,9 +1,50 @@
 "use client";
 
-export default function Terms() {
-  return (
-    <>
-      <h1>Terms & Conditions Page</h1>
-    </>
-  );
+import {useContext} from "react";
+import {useDataFetch} from "../../hooks/useDataFetch.jsx";
+import {pageStyle} from "../../styles/globalStyles.js";
+import {SingleEntryURL, TERMS_ID} from "../../utils/buildURL";
+import MainLayout from "../../MainLayout.jsx";
+import {PageCategoryContext} from "../../layout.js";
+import CustomSkeleton from "../../components/CustomSkeleton.jsx";
+import CustomBanner from "../../components/CustomBanner.jsx";
+import TeamMembers from "../../components/fragments/TeamMembers.jsx";
+import {Col, Divider, Image, Row} from "antd";
+import Certificates from "../../components/fragments/Certificates.jsx";
+import Title from "antd/es/typography/Title";
+import Paragraph from "antd/es/typography/Paragraph";
+
+export default function Shipping() {
+    let {pageCategory} = useContext(PageCategoryContext);
+    console.log(`${pageCategory} rendered`);
+
+    const {data, isLoading} = useDataFetch(SingleEntryURL(TERMS_ID));
+
+    return (<MainLayout>
+            <div style={pageStyle}>
+                {isLoading || !data ? (<CustomSkeleton/>) : (<>
+                        <Row>
+                            <Col xs={0} sm={0} md={24}>
+                                <Image
+                                    alt="Placeholder team picture"
+                                    src="https://placekitten.com/1800/400"
+                                    // width={1800}
+                                    // height={600}
+                                    preview={false}
+                                />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <Title>
+                                    {data.fields.title}
+                                </Title>
+                                <Paragraph>
+                                    {data.fields.body}
+                                </Paragraph>
+                            </Col>
+                        </Row>
+                    </>)}
+            </div>
+        </MainLayout>);
 }
--- app/utils/buildURL.jsx
+++ app/utils/buildURL.jsx
@@ -7,6 +7,8 @@ export const ABOUT_ID = `5wXQGH7Of5nYxWqPzLh8SM`;
 export const CONTACT_ID = `2UDWb179AgOuPSwqzLytSM`;
 export const DISTRIBUTORS_ID = `FT8DqdXArnbIvW1FKu9G5`;
 export const CAREERS_ID = `gcAgFCXjK19coibunSVg9`;
+export const SHIPPING_ID = `5cXZW5QSIYs2sMwnxCldXV`;
+export const TERMS_ID = `55R3KbwVCohVJKUFHssPGV`;
 
 // For a single entry based on its Contentful ID
 export function SingleEntryURL(entryID) {
