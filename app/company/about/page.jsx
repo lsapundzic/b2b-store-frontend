2f4e0a71863a0f8f83d4744aed33602876c62538
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-08-27 18:07:47 +0200

    Career page enabled

--- app/company/about/page.jsx
+++ app/company/about/page.jsx
@@ -1,51 +1,59 @@
 "use client";
 
-import { useContext } from "react";
-import { useDataFetch } from "../../hooks/useDataFetch.jsx";
-import { pageStyle } from "../../styles/globalStyles.js";
-import { ABOUT_ID, SingleEntryURL } from "../../utils/buildURL";
+import {useContext} from "react";
+import {useDataFetch} from "../../hooks/useDataFetch.jsx";
+import {pageStyle} from "../../styles/globalStyles.js";
+import {ABOUT_ID, SingleEntryURL} from "../../utils/buildURL";
 import MainLayout from "../../MainLayout.jsx";
-import { PageCategoryContext } from "../../layout.js";
+import {PageCategoryContext} from "../../layout.js";
 import CustomSkeleton from "../../components/CustomSkeleton.jsx";
 import CustomBanner from "../../components/CustomBanner.jsx";
 import TeamMembers from "../../components/fragments/TeamMembers.jsx";
-import { Col, Divider, Image, Row } from "antd";
+import {Col, Divider, Image, Row} from "antd";
 import Certificates from "../../components/fragments/Certificates.jsx";
 
 export default function About() {
-  let { pageCategory } = useContext(PageCategoryContext);
-  console.log(`${pageCategory} rendered`);
+    let {pageCategory} = useContext(PageCategoryContext);
+    console.log(`${pageCategory} rendered`);
 
-  const { data, isLoading } = useDataFetch(SingleEntryURL(ABOUT_ID));
+    const {data, isLoading} = useDataFetch(SingleEntryURL(ABOUT_ID));
 
-  return (
-    <MainLayout>
-      <div style={pageStyle}>
-        {isLoading || !data ? (
-          <CustomSkeleton />
-        ) : (
-          <>
-            <Row>
-              <Col xs={0} sm={0} md={24}>
-                <Image
-                  alt="Placeholder team picture"
-                  src="https://placekitten.com/1800/400"
-                  // width={1800}
-                  // height={600}
-                  preview={false}
-                />
-              </Col>
-            </Row>
-            <TeamMembers
-              componentTitle={data.fields.title}
-              componentBody={data.fields.body}
-            />
-            <CustomBanner bannerContent={""} />
-            <Divider orientation="left">Certificates</Divider>
-            <Certificates />
-          </>
-        )}
-      </div>
-    </MainLayout>
-  );
+    return (
+        <MainLayout>
+            <div style={pageStyle}>
+                {isLoading || !data ? (
+                    <CustomSkeleton/>
+                ) : (
+                    <>
+                        <Row>
+                            <Col xs={0} sm={0} md={24}>
+                                <Image
+                                    alt="Placeholder team picture"
+                                    src="https://placekitten.com/1800/400"
+                                    // width={1800}
+                                    // height={600}
+                                    preview={false}
+                                />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col span={24}>
+                                <TeamMembers
+                                    componentTitle={data.fields.title}
+                                    componentBody={data.fields.body}
+                                />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col span={24}>
+                                <CustomBanner bannerContent={""}/>
+                                <Divider orientation="left">Certificates</Divider>
+                                <Certificates/>
+                            </Col>
+                        </Row>
+                    </>
+                )}
+            </div>
+        </MainLayout>
+    );
 }
--- app/utils/buildURL.jsx
+++ app/utils/buildURL.jsx
@@ -6,27 +6,28 @@ const baseURL = "https://cdn.contentful.com";
 export const ABOUT_ID = `5wXQGH7Of5nYxWqPzLh8SM`;
 export const CONTACT_ID = `2UDWb179AgOuPSwqzLytSM`;
 export const DISTRIBUTORS_ID = `FT8DqdXArnbIvW1FKu9G5`;
+export const CAREERS_ID = `gcAgFCXjK19coibunSVg9`;
 
 // For a single entry based on its Contentful ID
 export function SingleEntryURL(entryID) {
-  const url = `${baseURL}/spaces/${process.env.NEXT_PUBLIC_SPACE_ID}/environments/master/entries/${entryID}?access_token=${process.env.NEXT_PUBLIC_DELIVERY_ACCESS_TOKEN}`;
-  return url;
+    const url = `${baseURL}/spaces/${process.env.NEXT_PUBLIC_SPACE_ID}/environments/master/entries/${entryID}?access_token=${process.env.NEXT_PUBLIC_DELIVERY_ACCESS_TOKEN}`;
+    return url;
 }
 
 // For all entries in the Contentful space
 export function AllEntriesURL() {
-  const url = `${baseURL}/spaces/${process.env.NEXT_PUBLIC_SPACE_ID}/environments/master/entries?access_token=${process.env.NEXT_PUBLIC_DELIVERY_ACCESS_TOKEN}`;
-  return url;
+    const url = `${baseURL}/spaces/${process.env.NEXT_PUBLIC_SPACE_ID}/environments/master/entries?access_token=${process.env.NEXT_PUBLIC_DELIVERY_ACCESS_TOKEN}`;
+    return url;
 }
 
 // For all entries of a specific content type
 export function ContentTypeEntriesURL(productType) {
-  const url = `${baseURL}/spaces/${process.env.NEXT_PUBLIC_SPACE_ID}/environments/master/entries?access_token=${process.env.NEXT_PUBLIC_DELIVERY_ACCESS_TOKEN}&content_type=${productType}`;
-  return url;
+    const url = `${baseURL}/spaces/${process.env.NEXT_PUBLIC_SPACE_ID}/environments/master/entries?access_token=${process.env.NEXT_PUBLIC_DELIVERY_ACCESS_TOKEN}&content_type=${productType}`;
+    return url;
 }
 
 // For assets like images and pdfs
 export function AssetURL(assetID) {
-  const url = `${baseURL}/spaces/${process.env.NEXT_PUBLIC_SPACE_ID}/environments/master/assets/${assetID}?access_token=${process.env.NEXT_PUBLIC_DELIVERY_ACCESS_TOKEN}`;
-  return url;
+    const url = `${baseURL}/spaces/${process.env.NEXT_PUBLIC_SPACE_ID}/environments/master/assets/${assetID}?access_token=${process.env.NEXT_PUBLIC_DELIVERY_ACCESS_TOKEN}`;
+    return url;
 }
