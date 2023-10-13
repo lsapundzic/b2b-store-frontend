010acbfc1b375b92c60387b1c732002db0c39c4e
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-10-13 19:25:02 +0200

    Branch for bachelor thesis without major changes

--- app/hooks/useDataFetch.jsx
+++ app/hooks/useDataFetch.jsx
@@ -1,5 +1,5 @@
 /*
-isLoading Use State has two purposes. 
+isLoading useState has two purposes. 
 
 1. Until the data is fetched there needs to be a placeholder, otherwise only initial state 
 will be passed to variables and will not be overwritten once fetch is complete. 
@@ -7,38 +7,38 @@ will be passed to variables and will not be overwritten once fetch is complete.
 2. It is used for controlling placeholder UI elements
 */
 
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 export function useDataFetch(url) {
-    const [data, setData] = useState(null);
-    const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await fetch(url);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(url);
 
-                if (!response.ok) {
-                    setData(false);
-                    throw new Error(
-                        `useDataRetriever msg: failed to fetch data. HTTP response status: ${response.status}`
-                    );
-                }
-
-                const fetchedData = await response.json();
-                setData(fetchedData);
-                setLoading(false);
-                console.log("useDataFetch msg: data fetched: ", fetchedData);
-            } catch (error) {
-                console.error(
-                    "useDataFetch msg: general error occurred while fetching data. Message: ",
-                    error
-                );
-            }
+        if (!response.ok) {
+          setData(false);
+          throw new Error(
+            `useDataRetriever msg: failed to fetch data. HTTP response status: ${response.status}`
+          );
         }
 
-        fetchData();
-    }, [url]);
+        const fetchedData = await response.json();
+        setData(fetchedData);
+        setLoading(false);
+        console.log("useDataFetch msg: data fetched: ", fetchedData);
+      } catch (error) {
+        console.error(
+          "useDataFetch msg: general error occurred while fetching data. Message: ",
+          error
+        );
+      }
+    }
 
-    return {data, isLoading};
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading };
 }
