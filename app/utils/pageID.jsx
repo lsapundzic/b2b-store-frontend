dbe1975f1433a50f05e662c2a6c3f16db78acc5a
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-11-19 12:35:49 +0100

    moved components outside of the app directory

--- app/utils/pageID.jsx
+++ app/utils/pageID.jsx
@@ -5,5 +5,3 @@ export const DISTRIBUTORS_ID = `FT8DqdXArnbIvW1FKu9G5`;
 export const CAREERS_ID = `gcAgFCXjK19coibunSVg9`;
 export const SHIPPING_ID = `5cXZW5QSIYs2sMwnxCldXV`;
 export const TERMS_ID = `55R3KbwVCohVJKUFHssPGV`;
-
-// tesrs
