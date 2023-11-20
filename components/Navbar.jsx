36bdeadc70cc53f82ebaeb7b118e97ed7ab2e98a
lsapundzic <30961251+lsapundzic@users.noreply.github.com>
2023-11-20 17:27:06 +0100

    Testing git

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 import Image from "next/image";
 import useWindowWidth from "@/app/hooks/useWindowWidth";
 
-// AntD icons
+// AntD icons AB
 import {
   AimOutlined,
 
